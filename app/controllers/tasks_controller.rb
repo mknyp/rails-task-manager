14fa23552e73d5d9961368ea12a86b91677fce66
Maximilian Knyphausen <maxiknyphausen@gmail.com>
2020-11-10 15:54:04 +0100

    all CRUD solved

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,46 @@
 class TasksController < ApplicationController
+
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to task_path(task) # there is no view so I need to forward to showpage of that task (task_path = route to show; task = instance)
+  end
+
+  def edit
+
+  end
+
+  def update
+    @task.update(task_params)
+
+    redirect_to task_path(@task) # Redirect to the show page
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  # STRONG PARAMS:
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # SYNTAX = verb path, to: controller#controller_action
+
+  # READ all tasks
+  get '/tasks', to: 'tasks#index' # we can have any path we want pointing to any controller action
+  # READ 1 task
+  get '/tasks/new', to: 'tasks#new' # display a new empty form for a new task (belongs to CREATE)
+  get '/tasks/:id', to: 'tasks#show', as: :task# reading an exisiting task by id
+  # CREATE 1 task
+  post '/tasks', to: 'tasks#create' # creates task upon clicking submit (its a convention from the form which always posts to index)
+  # # UPDATE 1 task
+  get '/tasks/:id/edit', to: 'tasks#edit' # edit page for prefilled form for a specific task ID
+  # patch '/tasks/:id', to: 'tasks#update'
+  # # DELETE 1 task
+  # delete '/tasks/:id', to: 'tasks#destroy'
 end
